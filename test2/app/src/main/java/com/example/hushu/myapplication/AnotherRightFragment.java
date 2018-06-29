889b8a4ea1f2f99c859c621206f0b2a41e9fb207
your name <942193225@qq.com>
2018-06-29 11:33:51 +0800

    哈喽

---
+++ test2/app/src/main/java/com/example/hushu/myapplication/AnotherRightFragment.java
@@ -0,0 +1,20 @@
+package com.example.hushu.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hushu on 2018/6/27.
+ */
+
+public class AnotherRightFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.another_right_fragment,container,false);
+        return view;
+    }
+}
